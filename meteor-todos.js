d8cb7c0563096b0c5377db24c3bc0befac9f578b
Joe Burdick <josephdburdick@gmail.com>
2014-11-09 21:16:37 -0500

    Add check off and delete functionality.

--- meteor-todos.js
+++ meteor-todos.js
@@ -25,6 +25,16 @@ if (Meteor.isClient) {
       return false;
     }
   });
+  Template.task.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite 
+      // of it's current value.
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  });
 }
 
 if (Meteor.isServer) {
